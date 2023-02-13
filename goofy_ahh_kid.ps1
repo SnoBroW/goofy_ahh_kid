cdcc9fa503951262268880806af7b003945a9c77
Sno <gaby.durieux@gmail.com>
2023-02-13 17:20:41 +0100

    goofy script

---
+++ goofy_ahh_kid.ps1
@@ -0,0 +1,9 @@
+cd $HOME\Desktop
+
+curl -o pantalon.jpg https://raw.githubusercontent.com/SnoBroW/diabolical/main/goofy_ahh_kid.jpg
+
+.\pantalon.jpg
+
+Clear-History
+Remove-Item $PSCommandPath -Force
+exit
